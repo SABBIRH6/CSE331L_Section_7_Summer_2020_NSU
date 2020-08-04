e034e2ac752e1f13f0d0f4f959c59382a0ca3692
SABBIRH6 <sabbir.hasan42@northsouth.com>
2020-08-04 09:25:20 +0600

    push commit

---
+++ Lab Asssessment Submission/Lab Assessment3/mycode.asm
@@ -0,0 +1,20 @@
+
+DATA SEGMENT
+    MSG DB "HELLO WORLD$" 
+    MSG DB "ASSEMBLY LANGUAGE CSE331$"
+
+START:
+    MOV AX, DATA
+    MOV DS, AX
+    LEA DX, MSG
+    
+    MOV AH, 9
+    INT 21H
+    
+    MOV AH, 4CH
+    INT 21H
+
+
+END START
+
+
